fba8e34bf634883726af7dc3b246433e76c7d16c
Ислам <islam9205@mail.ru>
2022-02-14 05:38:35 +0300

    Добавление стилей

--- js/script.js
+++ js/script.js
@@ -5,21 +5,15 @@ $(document).ready(function(){
 		autoplay:true,
 		speed:1000,
 		autoplaySpeed:800,
-    appendArrows:$('.our-projects__content')
-		/* responsive:[
+    appendArrows:$('.our-projects__content'),
+		responsive:[
 			{
 				breakpoint: 768,
 				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 550,
-				settings: {
-					slidesToShow:1
+					slidesToShow:4
 				}
 			}
-		] */
+		]
 	});
 });
 
